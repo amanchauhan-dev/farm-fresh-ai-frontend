b75c6d567fbabfec3973648ecd3065b1f3a70970
amanchauhan-dev <amanchauhan0435@gmail.com>
2025-08-07 17:31:42 +0530

    reverse

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,32 +1,29 @@
 
-// import {
-//     SidebarInset,
-//     SidebarProvider,
-// } from "@/components/ui/sidebar"
-// import { SiteHeader } from "./_components/site-header"
-// import { AppSidebar } from "./_components/app-sidebar"
-
-
-// export default function layout({ children }: { children: React.ReactNode }) {
-//     return (
-//         <SidebarProvider
-//             style={
-//                 {
-//                     "--sidebar-width": "calc(var(--spacing) * 72)",
-//                     "--header-height": "calc(var(--spacing) * 12)",
-//                 } as React.CSSProperties
-//             }
-//         >
-//             <AppSidebar variant="inset" />
-//             <SidebarInset>
-//                 <SiteHeader />
-//                 {children}
-//             </SidebarInset>
-//         </SidebarProvider>
-//     )
-// }
+import {
+    SidebarInset,
+    SidebarProvider,
+} from "@/components/ui/sidebar"
+import { SiteHeader } from "./_components/site-header"
+import { AppSidebar } from "./_components/app-sidebar"
 
 
 export default function layout({ children }: { children: React.ReactNode }) {
-    return children
+    return (
+        <SidebarProvider
+            style={
+                {
+                    "--sidebar-width": "calc(var(--spacing) * 72)",
+                    "--header-height": "calc(var(--spacing) * 12)",
+                } as React.CSSProperties
+            }
+        >
+            <AppSidebar variant="inset" />
+            <SidebarInset>
+                <SiteHeader />
+                {children}
+            </SidebarInset>
+        </SidebarProvider>
+    )
 }
+
+
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,22 @@
-// import React from 'react';
-// import { SectionCards } from './_components/section-cards';
-// import { ChartAreaInteractive } from './_components/chart-area-interactive';
-
-import { FarmDashboard } from "./_components/dashboard";
-
-// const Page = () => {
-//     return (
-//         <div className="flex flex-1 flex-col">
-//             <div className="@container/main flex flex-1 flex-col gap-2">
-//                 <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-//                     <SectionCards />
-//                     <div className="px-4 lg:px-6">
-//                         <ChartAreaInteractive />
-//                     </div>
-
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Page;
+import React from 'react';
+import { SectionCards } from './_components/section-cards';
+import { ChartAreaInteractive } from './_components/chart-area-interactive';
 
 
-export default function Home() {
-    return <FarmDashboard />
+const Page = () => {
+    return (
+        <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                    <SectionCards />
+                    <div className="px-4 lg:px-6">
+                        <ChartAreaInteractive />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 }
+
+export default Page;
